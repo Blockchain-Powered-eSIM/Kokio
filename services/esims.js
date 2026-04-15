972732b9e17860a5b23291333e45612cdf426939
dunegxn <arpitxdungeon@gmail.com>
2026-04-15 16:57:38 +0530

    +topUpCompatibility&Apply

--- services/esims.js
+++ services/esims.js
@@ -4,6 +4,7 @@ const API_PATHS = {
   FETCH_ESIMS: "/v1/catalogue",
   ORDER: "/v1/order",
   VALIDATE_COUPON: (couponCode) => `/v1/coupon/${couponCode}`,
+  ESIM_COMPATIBILITY: "/v1/esim/compatibility",
 };
 
 export const fetchEsimsCatalogue = (payload) =>
@@ -13,3 +14,8 @@ export const eSimOderCheckout = (payload) => api.post(API_PATHS.ORDER, payload);
 
 export const validateCoupon = (couponCode) =>
   api.get(API_PATHS.VALIDATE_COUPON(couponCode));
+
+export const checkEsimTopUpCompatibility = ({ deviceId, planId, esimId }) => {
+  const params = { deviceId, planId, ...(esimId && { esimId }) };
+  return api.get(API_PATHS.ESIM_COMPATIBILITY, params);
+};
